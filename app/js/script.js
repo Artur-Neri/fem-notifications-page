a61dcc5a7897db5751546e5ceeae5e8413c82705
Artur Neri <arturnerictt@gmail.com>
2024-08-15 14:00:03 -0300

    complete

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,28 @@
-console.log('I loaded!!');
+const notifications = document.querySelectorAll('.notification__container');
+const btnReadAll = document.querySelector('#readAll');
+const notificationNumber = document.querySelector('#notificationNumber');
+
+notifications.forEach(item => {
+  item.addEventListener('mouseenter', e => {
+    if (e.target.children[0].children[1].children[0].classList.contains('unread')){
+      console.log('mensagem lida');
+      e.target.children[0].children[1].children[0].classList.remove('unread');
+      updateNotificationNumber();
+    };
+  })
+})
+
+function readAllNotifications() {
+  let notifications = document.querySelectorAll('.notification__container');
+  notifications.forEach(item => {
+    item.children[0].children[1].children[0].classList.remove('unread');
+  });
+  updateNotificationNumber()
+}
+
+function updateNotificationNumber() {
+  const count = document.querySelectorAll('.unread').length;
+  notificationNumber.innerHTML = count;
+}
+
+btnReadAll.onclick = readAllNotifications;
